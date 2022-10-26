e7be28b71e403cb446679c636008cf83ab63bfde
CHHOA thierry <thierry.chhoa@etu.univ-paris-diderot.fr>
2022-10-26 21:12:16 +0200

    #16 first version modification user profile

--- src/front/userhome/userhome.module.ts
+++ src/front/userhome/userhome.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserhomeService } from './userhome.service';
 import { UserhomeController } from './userhome.controller';
-import { QuoteModule } from '../api/quote/quote.module';
+import { QuoteModule } from '../../api/quote/quote.module';
 
 @Module({
   providers: [UserhomeService],
